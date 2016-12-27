bf7f581c597d27de9e4f8cc808672a9cb076952f
Martin O Leary <moleary1987@gmail.com>
2016-12-27 09:36:12 +0000

    Adding index.js and Procfile for Heroku server - WIP for bot

--- sonos-controller.py
+++ sonos-controller.py
@@ -1,7 +1,53 @@
+import sys, getopt
 import soco
-for zone in soco.discover():
-    print zone.player_name
 
-zone_list = list(soco.discover())
-zone_list
-LR = zone_list[0]
+def getStatus(player):
+    return player.get_current_transport_info()['current_transport_state']
+    
+def printInfo(player):
+    info = player.get_current_track_info()
+    print "Status : %s" % player.get_current_transport_info()['current_transport_state']
+    print "Song : %s at %s" % (info['title'],info['position'])
+    
+def play(player):
+    player.play()
+    
+def pause(player):
+    player.pause()
+
+def next(player):
+    player.next()
+
+def main(argv):
+    # Get all devices in zone
+    zone_list = list(soco.discover())
+    # Only 1 system in zone - 'Family Room'
+    FR = zone_list[0]
+    try:
+        opts, args = getopt.getopt(argv,"np",["play","pause","status"])
+    except getopt.GetoptError:
+        print 'Expected options -n "next", -p "previous", --play,--pause'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-n':
+            print 'Playing next...'
+            FR.next()
+            FR.play()
+            printInfo(FR)
+        elif opt == '-p':
+            print 'Playing previous...'
+            FR.previous()
+            FR.play()
+            printInfo(FR)
+        elif opt == '--play':
+            print 'Playing...'
+            FR.play()
+            printInfo(FR)
+        elif opt == '--pause':
+            print 'Pausing...'
+            FR.pause()
+            printInfo(FR)
+        elif opt == '--status':
+            printInfo(FR)     
+if __name__ == "__main__":
+   main(main(sys.argv[1:]))
