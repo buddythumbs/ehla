955b8270a42783891b3a72ac65271cf6160ab0c3
Martin O Leary <moleary1987@gmail.com>
2016-12-29 21:45:33 +0000

    Adding media handler

--- js/messenger-handles.js
+++ js/messenger-handles.js
@@ -164,7 +164,7 @@ module.exports = {
         case "image":
         console.log('Image');
         if (attachment.payload.sticker_id) {
-          if (ttachment.payload.sticker_id === 369239263222822) {
+          if (attachment.payload.sticker_id === 369239263222822) {
             module.exports.sendTextMessage(sender, "beep boop 💻")
           }
         }
