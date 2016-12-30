5277472b356d019cc8148e650ecd9b9ffdba235f
Martin O Leary <moleary1987@gmail.com>
2016-12-30 19:44:00 +0000

    Refactoring messaging

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ const fbm = require('./messenger-handles');
 const weatherAPI = '8ba0a17ada98c62ad89a2f76f571960d'
 const location = 'Maynooth,ie'
 const token = "EAADhwQPQXKcBAHlW2N5TCSNdGfZAV6zseswplofZB0uK3nBsGZB0ZBJF2X21OExJCkGkxBQRTVWlKE0upHTGGfJCAVNTPx9SDv1Wzsem8RZCWULb2KEY7SS58w30zTvPpZAXVc8ZBzvBGZB23yOsxkpCN4fNo7ydbcD4acG0lFS4AwZDZD"
-const wUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+location +'&units=metric&APPID=' + weatherAPI
+let wUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+location +'&units=metric&APPID=' + weatherAPI
 
 module.exports = {
   getWeather : (location) => {
