87bf3785868a0f5c595f41f378256514e8b7fbb8
Martin O Leary <moleary1987@gmail.com>
2017-01-02 14:00:14 +0000

    Testing again

--- js/messenger-handles.js
+++ js/messenger-handles.js
@@ -118,11 +118,10 @@ var handleMessage = (messaging_event) => {
         if (text.match(/hey|hello|hi|👋/i)){
           let url = 'https://scontent.xx.fbcdn.net/v/t34.0-12/15870941_10207508265653446_564443884_n.gif?_nc_ad=z-m&oh=cb1ff0bece4af4b01ff3c00ae17ef8a5&oe=586CB6DA'
           let atts = 'image'
-          newMessage(sender,url,atts)
+          sendImg(sender,url)
           .then(typingOff(sender),(err)=>{})
           .then(newMessage(sender,"Hey " + user.first_name + "! \nWhat can I do for you ? ... beep boop",welcome)
           ,(err)=>{})
-          .then(newMessage(sender,url,atts),(err)=>{})
         }else if (text.toLowerCase() === "help") {
           newMessage(sender,"Help:\n Type 'Pic' to get back a picture\nType 'Hello/Hi/Hey' to get a response\n")
           typingOff(sender)
