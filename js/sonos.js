b15e2d6f45582d4c5a25914f061ef8e794c16e22
Martin O Leary <moleary1987@gmail.com>
2017-01-02 11:06:27 +0000

    Changing newMessage and layout of messenger file

--- js/sonos.js
+++ js/sonos.js
@@ -6,7 +6,7 @@ let handleRequest = (arg) =>{
     arg = "--play"
   }
   var spawn = require("child_process").spawn;
-  var process = spawn('python',["sonos-controller.py", arg]);
+  var process = spawn('python',["../sonos-controller.py", arg]);
   process.stdout.on('data', (data)=>{
     // Do something with the data returned from python script
     console.log("SONOS Reply :", data);
